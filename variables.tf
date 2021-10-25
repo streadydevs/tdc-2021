1b524133efea513d38dfcde7787e457628475922
Otto Lote <otto@lote.no>
2021-10-25 20:29:37 +0200

    Change default region to europe-north1 (Finland)

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "cluster_name" {
 
 variable "region" {
   description = "The region to host the cluster in"
-  default     = "us-central1"
+  default     = "europe-west1"
 }
 
 variable "zones" {
