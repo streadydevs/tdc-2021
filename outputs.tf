b80e4f65a21127e7977837a459c0bb51201d99e6
Otto Lote <otto@lote.no>
2021-10-25 18:21:08 +0200

    Remove unnecessary outputs

--- outputs.tf
+++ outputs.tf
@@ -14,28 +14,6 @@
  * limitations under the License.
  */
 
-output "kubernetes_endpoint" {
-  description = "The cluster endpoint"
-  sensitive   = true
-  value       = module.gke.endpoint
-}
-
-output "client_token" {
-  description = "The bearer token for auth"
-  sensitive   = true
-  value       = base64encode(data.google_client_config.default.access_token)
-}
-
-output "ca_certificate" {
-  description = "The cluster ca certificate (base64 encoded)"
-  value       = module.gke.ca_certificate
-}
-
-output "service_account" {
-  description = "The default service account used for running nodes."
-  value       = module.gke.service_account
-}
-
 output "cluster_name" {
   description = "Cluster name"
   value       = module.gke.name
